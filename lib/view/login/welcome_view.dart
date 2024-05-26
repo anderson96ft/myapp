45e9fac8c7e3007aa40e242eb33d5a51d7cb7595
anderson96ft1 <anderson96ft1@gmail.com>
2024-05-26 18:16:33 +0000

    first commit

---
+++ lib/view/login/welcome_view.dart
@@ -0,0 +1,95 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:myapp/view/main_tabview/main_tabview.dart';
+
+import '../../common/color_extension.dart';
+import '../../common_widget/round_button.dart';
+
+class WelcomeView extends StatefulWidget {
+  const WelcomeView({super.key});
+
+  @override
+  State<WelcomeView> createState() => _WelcomeViewState();
+}
+
+class _WelcomeViewState extends State<WelcomeView> {
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Image.asset(
+                  "assets/img/welcome_top_shape.png",
+                  width: media.width,
+                ),
+                Image.asset(
+                  "assets/img/app_logo.png",
+                  width: media.width * 0.55,
+                  height: media.width * 0.55,
+                  fit: BoxFit.contain,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: media.width * 0.1,
+            ),
+            Text(
+              "Ordena, reserva y paga con\nnuestro menu digital\nen tu celular",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: TColor.secondaryText,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: media.width * 0.1,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: RoundButton(
+                title: "MENÚ",
+                onPressed: () async {
+                  try {
+                    await Firebase.initializeApp();
+                    final userCredential =
+                        await FirebaseAuth.instance.signInAnonymously();
+                    print("Signed in with temporary account.");
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const MainTabView(),
+                    ),
+                  );
+                  } on FirebaseAuthException catch (e) {
+                    switch (e.code) {
+                      case "operation-not-allowed":
+                        print(
+                            "Anonymous auth hasn't been enabled for this project.");
+                        break;
+                      default:
+                        print("Unknown error.");
+                    }
+                  }
+                  
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
